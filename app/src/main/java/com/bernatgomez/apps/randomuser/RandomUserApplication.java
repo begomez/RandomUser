a9cab970b957d1af737e761fecffb07463fb65f7
Bernat Gómez <bernat.gomez@gmail.com>
2017-09-08 03:15:58 +0200

    Create application class

---
+++ app/src/main/java/com/bernatgomez/apps/randomuser/RandomUserApplication.java
@@ -0,0 +1,34 @@
+package com.bernatgomez.apps.randomuser;
+
+import android.app.Application;
+
+import com.bernatgomez.apps.randomuser.utils.AndroidLogger;
+
+public class RandomUserApplication extends Application {
+
+    private static final String TAG = RandomUserApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AndroidLogger.logMsg(TAG, "onCreate()");
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+
+        AndroidLogger.logMsg(TAG, "onTrimMemory()");
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        AndroidLogger.logMsg(TAG, "onTerminate()");
+    }
+
+
+}
