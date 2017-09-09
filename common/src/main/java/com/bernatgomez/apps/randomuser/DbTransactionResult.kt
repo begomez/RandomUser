a8001e42d7fb1f9755ef12d8e7c90c083f24c862
Bernat Gómez <bernat.gomez@gmail.com>
2017-09-09 21:59:09 +0200

    Add model for db transaction results

---
+++ common/src/main/java/com/bernatgomez/apps/randomuser/DbTransactionResult.kt
@@ -0,0 +1,18 @@
+package com.bernatgomez.apps.randomuser
+
+
+import com.bernatgomez.apps.randomuser.models.BaseModel
+
+
+/**
+ * App transaction for db operation results
+ *
+ * Created by bernatgomez on 09/09/2017.
+ */
+
+data class DbTransactionResult(
+        var operationName : String, var operationSuccess: Boolean) : BaseModel {
+
+    constructor() : this("", false);
+
+}
