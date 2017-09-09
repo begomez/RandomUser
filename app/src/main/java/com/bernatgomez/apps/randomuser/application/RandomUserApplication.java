3cb7723b65652f94e43c9eeff08a6497fc545b78
Bernat Gómez <bernat.gomez@gmail.com>
2017-09-09 20:25:57 +0200

    Create db when applications starts

--- app/src/main/java/com/bernatgomez/apps/randomuser/application/RandomUserApplication.java
+++ app/src/main/java/com/bernatgomez/apps/randomuser/application/RandomUserApplication.java
@@ -1,34 +1,92 @@
 package com.bernatgomez.apps.randomuser.application;
 
 import android.app.Application;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.os.AsyncTask;
 
+import com.bernatgomez.apps.randomuser.persist.RandomUserDatabase;
 import com.bernatgomez.apps.randomuser.utils.AndroidLogger;
 
 public class RandomUserApplication extends Application {
 
     private static final String TAG = RandomUserApplication.class.getSimpleName();
 
+    private RoomDatabase database;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// LIFE CYCLE
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
     @Override
     public void onCreate() {
+        AndroidLogger.logMsg(TAG, "onCreate()");
+
         super.onCreate();
 
-        AndroidLogger.logMsg(TAG, "onCreate()");
+        this.createRepo();
+    }
+
+    private void createRepo() {
+        new DBCreator().execute();
     }
 
     @Override
     public void onTrimMemory(int level) {
-        super.onTrimMemory(level);
-
         AndroidLogger.logMsg(TAG, "onTrimMemory()");
 
+        super.onTrimMemory(level);
     }
 
     @Override
     public void onTerminate() {
+        AndroidLogger.logMsg(TAG, "onTerminate()");
+
         super.onTerminate();
 
-        AndroidLogger.logMsg(TAG, "onTerminate()");
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public boolean isDbCreated() {
+        return this.database != null;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// INNER CLASSES
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Async task used to create random user repo
+     */
+    final class DBCreator extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            AndroidLogger.logMsg(TAG, "doInBackground()");
+
+            try {
+                database =
+                    Room.databaseBuilder(getApplicationContext(), RandomUserDatabase.class, RandomUserDatabase.DB_NAME).build();
+
+                return true;
+
+            } catch (Exception e) {
+                AndroidLogger.logError(TAG, "doInBackground()", e);
+
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            AndroidLogger.logMsg(TAG, "onPostExecute(): " + result);
+        }
+    }
 
 }
